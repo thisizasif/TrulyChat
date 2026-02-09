299dd77b6828515ed8f71932241e11f1cd959e94
thisizasif <68771679+thisizasif@users.noreply.github.com>
2026-02-09 18:04:01 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Global settings for TrulyChat.
-// Change this number to set the maximum allowed channel number.
-// Set to 0 to allow any channel up to 9999.
-window.TRULYCHAT_MAX_CHANNEL_NUMBER = 1000;
